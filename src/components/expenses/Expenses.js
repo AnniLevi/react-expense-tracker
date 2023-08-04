2a65fb1c1259f782792a1b7951cd3111733fe0ff
AnniLevi <al.devpy@gmail.com>
2023-08-04 20:28:02 +0200

    refactoring of the Expenses component with a map function

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -5,10 +5,9 @@ import Card from "../ui/Card";
 function Expenses({ items }) {
   return (
     <Card className="expenses">
-      <ExpenseItem expense={items[0]} />
-      <ExpenseItem expense={items[1]} />
-      <ExpenseItem expense={items[2]} />
-      <ExpenseItem expense={items[3]} />
+      {items.map((item) => (
+        <ExpenseItem expense={item} key={item.id} />
+      ))}
     </Card>
   );
 }
